c85e7d91fc19392364c9368ea8ec7f5ba21d7e72
Yatin Sarbalia <yatinsns@gmail.com>
2014-04-05 13:28:51 +0530

    First initial commit

---
+++ HungryDog/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  HungryDog
+//
+//  Created by Yatin Sarbalia on 4/5/14.
+//  Copyright (c) 2014 Yatin Sarbalia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
