9dd9ec9fd206f7c2ec3803cb0d41ee73157fcaa8
Yatin Sarbalia <yatinsns@gmail.com>
2014-04-06 18:00:58 +0530

    MainScene: Add name label

--- HungryDog/MainScene.m
+++ HungryDog/MainScene.m
@@ -7,14 +7,33 @@
 //
 
 #import "MainScene.h"
+#import "SKLabelNode+HungryDogAdditions.h"
+#import "UIConstants.h"
+#import "UIUtils.h"
+
+const CGFloat NameLabelFontSize_iPhone = 30;
+const CGFloat NameLabelFontSize_iPad = 80;
 
 @implementation MainScene
 
 - (id)initWithSize:(CGSize)size {
   if (self = [super initWithSize:size]) {
     self.backgroundColor = [SKColor blackColor];
+    [self addNameLabel];
   }
   return self;
+}
+
+- (void)addNameLabel {
+  CGFloat fontSize = ValueForDevice(NameLabelFontSize_iPhone, NameLabelFontSize_iPad);
+  SKLabelNode *nameLabel = [SKLabelNode labelNodeWithFontNamed:AppFontName
+                                                      fontSize:fontSize
+                                                     fontColor:[SKColor whiteColor]];
+  nameLabel.text = AppName;
+  nameLabel.position = CGPointMake(self.size.width / 2,
+                                   self.size.height / 2);
+  nameLabel.verticalAlignmentMode = SKLabelVerticalAlignmentModeCenter;
+  [self addChild:nameLabel];
 }
 
 @end
