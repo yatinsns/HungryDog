0f7e601a0cc9059558eeb4d4c13b43850f6b8c9f
Yatin Sarbalia <yatinsns@gmail.com>
2014-04-06 18:47:42 +0530

    Add empty GameScene

---
+++ HungryDog/GameScene.h
@@ -0,0 +1,16 @@
+//
+//  GameScene.h
+//  HungryDog
+//
+//  Created by Yatin Sarbalia on 4/6/14.
+//  Copyright (c) 2014 Yatin Sarbalia. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+/**
+ This class represents scene for presenting game.
+ */
+@interface GameScene : SKScene
+
+@end
---
+++ HungryDog/GameScene.m
@@ -0,0 +1,20 @@
+//
+//  GameScene.m
+//  HungryDog
+//
+//  Created by Yatin Sarbalia on 4/6/14.
+//  Copyright (c) 2014 Yatin Sarbalia. All rights reserved.
+//
+
+#import "GameScene.h"
+
+@implementation GameScene
+
+- (id)initWithSize:(CGSize)size {
+  if (self = [super initWithSize:size]) {
+    self.backgroundColor = [SKColor blackColor];
+  }
+  return self;
+}
+
+@end
