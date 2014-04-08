54ec530cbef92762f325cbf8430c6017aa04eae0
yatinessnessapps <yatin@essnessapps.com>
2014-04-08 23:55:06 +0530

    GameScene: Add score label

--- HungryDog/GameScene.m
+++ HungryDog/GameScene.m
@@ -7,14 +7,42 @@
 //
 
 #import "GameScene.h"
+#import "SKLabelNode+HungryDogAdditions.h"
+#import "UIConstants.h"
+#import "UIUtils.h"
+
+const CGFloat ScoreLabelFontSize_iPhone = 20;
+const CGFloat ScoreLabelFontSize_iPad = 40;
+const CGFloat ScoreLabelPaddingRight = 10;
+const CGFloat ScoreLabelPaddingTop = 10;
+
+@interface GameScene ()
+
+@property (nonatomic) SKLabelNode *scoreLabel;
+
+@end
 
 @implementation GameScene
 
 - (id)initWithSize:(CGSize)size {
   if (self = [super initWithSize:size]) {
     self.backgroundColor = [SKColor blackColor];
+    [self addScoreLabel];
   }
   return self;
+}
+
+- (void)addScoreLabel {
+  CGFloat fontSize = ValueForDevice(ScoreLabelFontSize_iPhone, ScoreLabelFontSize_iPad);
+  _scoreLabel = [SKLabelNode labelNodeWithFontNamed:AppFontName
+                                          fontSize:fontSize
+                                         fontColor:[SKColor whiteColor]];
+  _scoreLabel.text = @"Score: 0";
+  _scoreLabel.verticalAlignmentMode = SKLabelVerticalAlignmentModeCenter;
+  _scoreLabel.horizontalAlignmentMode = SKLabelHorizontalAlignmentModeRight;
+  _scoreLabel.position = CGPointMake(self.size.width - ScoreLabelPaddingRight,
+                                     self.size.height - _scoreLabel.frame.size.height - ScoreLabelPaddingTop);
+  [self addChild:_scoreLabel];
 }
 
 @end
