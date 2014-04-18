c68ede9163f381d17fbc058930a3876dabecfd0f
yatinessnessapps <yatin@essnessapps.com>
2014-04-18 17:48:23 +0530

    Add CatcherMode

---
+++ HungryDog/CatcherMode.h
@@ -0,0 +1,18 @@
+//
+//  CatcherMode.h
+//  HungryDog
+//
+//  Created by Yatin Sarbalia on 4/18/14.
+//  Copyright (c) 2014 Yatin Sarbalia. All rights reserved.
+//
+
+#ifndef HungryDog_CatcherMode_h
+#define HungryDog_CatcherMode_h
+
+typedef NS_ENUM(NSUInteger, CatcherMode) {
+  CatcherModeRandom,
+  CatcherModePattern,
+  CatcherModeChase
+};
+
+#endif
